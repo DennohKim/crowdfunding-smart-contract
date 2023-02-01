d6b52da9d7db6973617eae29c1751079414a2f7f
DennohKim <kimathidennis38@gmail.com>
2023-02-01 20:30:40 +0300

    chore: add assets

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
+
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
+const activeChainId = ChainId.Goerli;
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <ThirdwebProvider desiredChainId={activeChainId}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </ThirdwebProvider>
   </React.StrictMode>
 );
