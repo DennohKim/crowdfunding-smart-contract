73349f71e0dd7b4a8562a3a199c0405ebce953cf
DennohKim <kimathidennis38@gmail.com>
2023-02-01 21:32:05 +0300

    chore: set up tailwind css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
-export default function Home() {
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
+
+
+export default function App() {
   return (
-    <div className="">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row ">
+      <div className="sm:flex hidden mr-10 relative">
+        Sidebar
+      </div>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5 ">
+        Navbar
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </div>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        epilogue: ['Epilogue', 'sans-serif'],
+      },
+      boxShadow: {
+        secondary: '10px 10px 20px rgba(2, 2, 2, 0.25)',
+      },
+    },
   },
   plugins: [],
 }
